a3d91e00120c4c123754f6a26ea683d54735f0f8
pavel dolgushin <pfdolgushin@gmail.com>
2023-01-04 23:33:13 +0500

    Вынесены параметры в  test_signup_new_account.py.py

--- fixture/application.py
+++ fixture/application.py
@@ -11,7 +11,7 @@ import string
 
 
 class Application:
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == 'chrome':
             chrome_options = webdriver.ChromeOptions()
             chrome_options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
@@ -25,8 +25,8 @@ class Application:
         #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
-        #self.contact = ContactHelper(self)
-        self.base_url=base_url
+        self.config = config
+        self.base_url = config["web"]["baseUrl"]
 
     def is_valid(self):
         try:
