57266cc2a247dc7dac70d47c16be5a8b5533c738
pavel dolgushin <pfdolgushin@gmail.com>
2023-01-08 18:20:36 +0500

    Тест с использованием Pop3

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 #from fixture.contact import ContactHelper
 from selenium.webdriver.support.ui import Select
 
@@ -25,6 +27,8 @@ class Application:
         #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.config = config
         self.base_url = config["web"]["baseUrl"]
 
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,7 +1,18 @@
+import string
+import random
+import time
 
-
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
 
 def test_signup_new_account(app):
-    username = 'user111'
-    password = 'test'
+    username = random_username("user_", 10)
+    email = username + "@localhost"
+    password = "test1234"
     app.james.ensure_user_exist(username, password)
+    app.signup.new_user(username, email, password)
+    app.session.login(username, password)
+    time.sleep(2)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
